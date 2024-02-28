b15978821eeabc691693fc5b17a5d8e58794fac0
Sergio Triana Escobedo <stescobedo.31@gmail.com>
2024-02-28 10:50:45 -0600

    Adding logic for register view

--- rest-django/server/views.py
+++ rest-django/server/views.py
@@ -1,5 +1,9 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from .serializers import UserSerializer
+from django.contrib.auth.models import User
+from rest_framework.authtoken.models import Token
+from rest_framework import status
 
 @api_view(['POST'])
 def login(request):
@@ -7,7 +11,17 @@ def login(request):
 
 @api_view(['POST'])
 def register(request):
-    return Response({})
+    serializer = UserSerializer(data = request.data)
+
+    if serializer.is_valid():
+        serializer.save()
+        user = User.objects.get(username=serializer.data['username'])
+        user.set_password(serializer.data['password'])
+        user.save()
+        token = Token.objects.create(user=user)
+        return Response({'token': token.key, "user": serializer.data}, status=status.HTTP_201_CREATED)
+
+    return Response({serializer.errors}, status=status.HTTP_400_BAD_REQUEST)
 
 @api_view(['POST'])
 def profile(request):
