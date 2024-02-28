d48743c758bce713be10aa8eb50e49daf3808b40
Sergio Triana Escobedo <stescobedo.31@gmail.com>
2024-02-28 10:34:15 -0600

    Add login, register and profile urls

--- rest-django/server/urls.py
+++ rest-django/server/urls.py
@@ -15,8 +15,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, re_path
+from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    re_path('login', views.login),
+    re_path('register', views.register),
+    re_path('profile', views.profile),
 ]
